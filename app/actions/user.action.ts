85b67f67832da923ad79709e687fa4eeb2df8a7a
asia272 <asiaashraf7272@gmail.com>
2026-04-25 16:55:57 +0500

    update suggest user action

--- app/actions/user.action.ts
+++ app/actions/user.action.ts
@@ -99,7 +99,6 @@ export async function getRandomUsers() {
                     }
                 }
             },
-            take: 3,
         })
         return randomUsers;
     } catch (error) {
